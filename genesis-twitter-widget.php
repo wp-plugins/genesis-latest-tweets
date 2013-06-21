a18cfa6d87fdea2f7747fce3415202d4079fad5d
wpsmith <wpsmith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-21 15:47:53 +0000

    version bump
    git-svn-id: https://plugins.svn.wordpress.org/genesis-latest-tweets/trunk@729455 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- genesis-twitter-widget.php
+++ genesis-twitter-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Genesis Latest Tweets Widget
 Plugin URI: http://wpsmith.net/
 Description: Genesis Latest Tweets Widget.
-Version: 1.1.1
+Version: 1.1.2
 Author: wpsmith,Nick_theGeek
 Author URI: http://wpsmith.net/
 Author Email: t@wpsmith.net 
